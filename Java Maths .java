30580393ae11155ce2cafbf8ddfcbcd951bb0a03
Tabassum Ridy <tabassumridy461@gmail.com>
2026-06-21 21:54:29 +0600

    Create Java Maths .java

---
+++ Java Maths .java
@@ -0,0 +1,24 @@
+public class Maths {
+    public static void main(String[] args) {
+
+        // Math.max()
+        System.out.println("Max: " + Math.max(5, 10));
+
+        // Math.min()
+        System.out.println("Min: " + Math.min(5, 10));
+
+        // Math.sqrt()
+        System.out.println("Square Root: " + Math.sqrt(64));
+
+        // Math.abs()
+        System.out.println("Absolute Value: " + Math.abs(-4.7));
+
+        // Math.pow()
+        System.out.println("Power: " + Math.pow(2, 8));
+
+        // Rounding Methods
+        System.out.println("Round: " + Math.round(4.6));
+        System.out.println("Ceil: " + Math.ceil(4.1));
+        System.out.println("Floor: " + Math.floor(4.9));
+    }
+}
