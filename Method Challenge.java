1f22b4396e219b185157405da6cc4ec82368bd0f
Tabassum Ridy <tabassumridy461@gmail.com>
2026-06-21 22:16:49 +0600

    Create Method Challenge.java

---
+++ Method Challenge.java
@@ -0,0 +1,13 @@
+public class MethodChallenge {
+
+    // Static method
+    static void welcomeMessage() {
+        System.out.println("Welcome to Java!");
+    }
+
+    public static void main(String[] args) {
+
+        // Call the method
+        welcomeMessage();
+    }
+}
